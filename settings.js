63d2613390322892cab6d0ed4953cbbe4de6ac77
Janithmaduwa <148183803+Janithmaduwa@users.noreply.github.com>
2023-11-27 09:27:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['94760618082'] //ur owner number
+global.ownernomer = "94760618092" //ur owner number2
+global.ownername = "🦄 MR.JANIYA" //ur owner name
+global.ytname = "YT: 😪" //ur yt chanel name
+global.socialm = "GitHub: Janithmaduwa" //ur github or insta name
+global.location = "Srilanka, Monaragala, Badalkumbura" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
+global.botname = "JANIYA MD"
+global.ownernumber = '94760618092'
+global.ownername = 'MR.JANIYA
+global.ownerNumber = ["94760618092@s.whatsapp.net"]
+global.ownerweb = "https://janiyaofficial.github.io"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.wm = "Mr.Janiya."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.packname = "ජනියගෙ තමයි බන් 👽"
+global.author = "🦄MR.JANIYA\n\n"
+global.creator = "94760618092@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
